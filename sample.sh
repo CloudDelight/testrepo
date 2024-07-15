88955408d6132f0101d4e07dfd685f4d609ebb94
CloudDelight <clouddelight4knowledge@gmail.com>
2024-07-15 12:34:40 +0530

    added sample.sh file

---
+++ sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Type your name here"
+read name
+echo "Welcome $name"
+echo "Hello $USER..."
+echo "Your Home directory is $HOME"
+echo "Your shell is $SHELL"
+#adding new line to check git
